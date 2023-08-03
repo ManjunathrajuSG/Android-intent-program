978c138f9359702e7dcb5bdb7b064554caba7ea9
Manjunath Raju S G <132266851+ManjunathrajuSG@users.noreply.github.com>
2023-08-03 23:21:39 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -8,14 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    {
     public static final String Efn = "com.example.MyProgram2Intent.Efn";
     public static final String Eusn = "com.example.MyProgram2Intent.Eusn";
     public static final String Ed = "com.example.MyProgram2Intent.Ed";
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.btn1);
